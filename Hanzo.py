acc32655413bc79f01a702d9fc30259cf5728345
marcusfaust <marcus.faust1@gmail.com>
2014-11-13 05:52:16 -0800

    modified:   Hanzo.py 	modified:   config.py 	new file:   dhcp_options.conf

--- Hanzo.py
+++ Hanzo.py
@@ -1,9 +1,9 @@
+import os
 import re
 import requests
 import urllib
 import json
 from config import *
-
 
 
 class RazorSession:
@@ -98,6 +98,28 @@ class RazorSession:
         return results
 
 
+    def writeDhcpOptions(self, file, nodelist):
+
+        nodeinfo = self.getNodes()
+
+        with open(file, 'w+') as f:
+            f.write('use-host-decl-names on;\n')
+            f.write(('option routers ' + self.esxi_default_gw + ';\n'))
+            f.write(('option subnet-mask ' + self.esxi_subnetmask + ';\n'))
+            f.write(('option domain-name-servers ' + self.esxi_dns + ';\n'))
+            f.write(('option domain-name ' + self.esxi_domain_suffix + ';\n'))
+
+            for node in nodelist:
+                f.write('\n')
+                f.write('host ' + nodeinfo[node]['metadata']['HZ_hostname'] + ' {\n')
+                f.write('\thardware ethernet ' + nodeinfo[node]['facts']['macaddress'] + ';\n')
+                f.write('\tfixed-address ' + nodeinfo[node]['metadata']['HZ_ipaddress'] + ';\n')
+                f.write('}\n')
+
+        f.closed
+
+
+
 def extractLastOctet(ipaddress):
     last = re.split(r'(.*)\.(.*)\.(.*)\.(.*)', ipaddress)
     str_last = str(last[-2])
@@ -107,10 +129,22 @@ def extractSubnet(ipaddress):
     subnet = re.split(r'(.*\..*\..*\.).*', ipaddress)
     return subnet[-2]
 
+def restartDHCP():
+    print "Removing DHCP lease files\n"
+    os.system('sudo rm -rf /var/lib/dhcp/*')
+    print "Restarting ISC DHCP Service\n"
+    os.system('sudo service isc-dhcp-server restart')
+
 
 if __name__ == '__main__':
 
     razor = RazorSession(RAZOR_REST_URL)
-    razor.getNodesWithRU()
+    nodelist = razor.getNodesWithRU()
+    razor.esxi_default_gw = '10.10.10.1'
+    razor.esxi_subnetmask = '255.255.255.0'
+    razor.esxi_dns = '10.10.10.5'
+    razor.esxi_domain_suffix = 'ssnsgs.net'
+    razor.writeDhcpOptions(DHCP_OPTIONS_FILE, nodelist)
+    print "hello"
 
 
